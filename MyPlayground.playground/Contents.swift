4c92ec9080defa4b47f4bdadb046bad7ef72869c
Carlos Júnior <carlosjunior@Carloss-iMac.local>
2018-12-09 16:50:23 -0200

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+
+print ("")
+
+func imprimirFicha(){
+    print("******")
+    print("******")
+    print("Nome : José da Silva")
+}
